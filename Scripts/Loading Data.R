9286446aca9e8bee50909492b760f2a7a5918f05
Charles-Bryan <charles.c.b2@outlook.com>
2019-08-18 13:24:14 -0400

    Updated the commented text but no funcitonal changes

--- Scripts/Loading Data.R
+++ Scripts/Loading Data.R
@@ -1,13 +1,5 @@
-# The purpose of this project is to demonstrate your ability to collect, work with, and clean
-# a data set. The goal is to prepare tidy data that can be used for later analysis. 
-# You will be graded by your peers on a series of yes/no questions related to the project. 
-# You will be required to submit: 
-#         1) a tidy data set as described below, 
-#         2) a link to a Github repository with your script for performing the analysis, and 
-#         3) a code book that describes the variables, the data, and any transformations or work 
-#                 that you performed to clean up the data called CodeBook.md. 
-#         4)You should also include a README.md in the repo with your scripts. 
-#                 This repo explains how all of the scripts work and how they are connected.
+## This script loads the data for this project which can be found at the following link:
+#       http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
 # Check if the data has been set up already
 if(!file.exists("./data")){
@@ -25,4 +17,3 @@ if(!file.exists("./data")){
         # Remove the zip file
         unlink("./data.zip")
 }
-
